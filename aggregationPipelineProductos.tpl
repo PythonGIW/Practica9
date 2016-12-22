4ce85b481d85632b1845443cbbb90b75bf0a52f3
juanmont <juanmont@ucm.es>
2016-12-22 16:24:07 +0100

    Apartados 1 y 2 y TPLs

---
+++ aggregationPipelineProductos.tpl
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html xml:lang="es">
+
+<head>
+  <title>Practica7</title>
+  <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+  <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body>
+  <div id="main">
+    <div id="header">
+      <div id="logo">
+        <h1>AGGREGATION PIPELINE</h1>
+      </div>
+    </div>
+    <!--
+    ESTA TABLA DEBE RECIBIR LOS SIGUIENTES DATOS
+        - nombres: Nombres de las columnas a mostrar
+        - result: resultados obtenidos para mostrar en la tabla
+        - claves: claves de acceso a los resulados r
+        - num: numero de resultados
+    -->
+    <div id="site_content">
+      	<div id="content">
+          <table>
+            <tr>
+            %for nombre in nombres:
+              <td><strong>{{nombre}}</strong></td>
+            %end
+            </tr>
+          %for r in result:
+            <tr>
+              <td>{{r["_id"]["nombre"]}}</td>
+              <td>{{r["num"]}}</td>
+              <td>{{r["_id"]["precio"]}}</td>
+            </tr>
+          %end 
+          </table>
+          <h1> Numero de resultados: {{num}} </h1>
+    	</div>
+    </div>
+</body>
+</html>
---
+++ consultas.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+ 
+##
+
+# Autores: 
+# Alberto Marquez Gomez
+# Álvaro Asenjo Torrijo
+# Juan Jose Montiel Cano
+# declaramos que esta solución
+# es fruto exclusivamente de nuestro trabajo personal. No hemos sido
+# ayudados por ninguna otra persona ni hemos obtenido la solución de
+# fuentes externas, y tampoco hemos compartido nuestra solución con
+# nadie. Declaramos además que no hemos realizado de manera desho-
+# nesta ninguna otra actividad que pueda mejorar nuestros resultados
+# ni perjudicar los resultados de los demás.
+
+## 
+
+
+from bottle import get, run, template, error, static_file, request,response,redirect
+from pymongo import MongoClient
+
+mongoclient = MongoClient()
+db = mongoclient['giw']
+
+
+@get('/top_countries')
+# http://localhost:8080/top_countries?n=3
+def agg1():
+	result = db.usuarios.aggregate( [
+		{"$group": {"_id": "$pais","num": {"$sum":1}}},
+ 		{"$sort" : {"num":-1, "_id":-1}},
+ 		{"$limit" : int(request.query['n'])}
+	])
+    	return template('aggregationPipeline.tpl', result = result, claves= ["_id","num"], nombres = ["Pais", "Numero"], num = num)
+
+@get('/products')
+# http://localhost:8080/products?min=2.34
+def agg2():
+	result2 = db.pedidos.aggregate([
+		{"$unwind":"$lineas"},
+		# {"$project": {"precio": "$lineas.precio",
+		# 				"cantidad": "$lineas.cantidad",
+		# 				"nombre": "$lineas.nombre"}},
+		{"$match" : {"lineas.precio":{"$gte":float(request.query['min'])}}},
+		{"$group": {"_id": { "nombre": "$lineas.nombre", "precio": "$lineas.precio"},"num": {"$sum":"$lineas.cantidad"}}}
+		#
+	])
+    	return template('aggregationPipelineProductos.tpl', result = result2, nombres = ["Nombre", "Numero", "Precio"], num = 10)
+
+    
+@get('/age_range')
+# http://localhost:8080/age_range?min=80
+def agg3():
+    pass
+    
+    
+@get('/avg_lines')
+# http://localhost:8080/avg_lines
+def agg4():
+    pass
+    
+    
+@get('/total_country')
+# http://localhost:8080/total_country?c=Alemania
+def agg5():
+    pass
+    
+        
+if __name__ == "__main__":
+    # No cambiar host ni port ni debug
+    run(host='localhost',port=8080,debug=True)
