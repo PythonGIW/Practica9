1206efc009b3567ff74e37efd4e70190b30a8839
juanmont <juanmont@ucm.es>
2017-01-08 13:18:19 +0100

    arreglado error

--- consultas.py
+++ consultas.py
@@ -29,39 +29,36 @@ db = mongoclient['giw']
 def agg1():
 	num = int(request.query['n'])
 	result = db.usuarios.aggregate( [
-		{"$group": {"_id": "$pais","num": {"$sum":1}}},
- 		{"$sort" : {"num":-1, "_id":-1}},
- 		{"$limit" : num}
-	])
-    	return template('aggregationPipeline.tpl', result = result, claves= ["_id","num"], nombres = ["Pais", "Numero"], num = num)
+			{"$group": {"_id": "$pais","num": {"$sum":1}}},
+	 		{"$sort" : {"num":-1, "_id":-1}},
+	 		{"$limit" : num}])
+	return template('aggregationPipeline.tpl', result = result, claves= ["_id","num"], nombres = ["Pais", "Numero"], num = num)
 
 @get('/products')
 # http://localhost:8080/products?min=2.34
 def agg2():
 	result2 = db.pedidos.aggregate([
-		{"$unwind":"$lineas"},
-		# {"$project": {"precio": "$lineas.precio",
-		# 				"cantidad": "$lineas.cantidad",
-		# 				"nombre": "$lineas.nombre"}},
-		{"$match" : {"lineas.precio":{"$gte":float(request.query['min'])}}},
-		{"$group": {"_id": { "nombre": "$lineas.nombre", "precio": "$lineas.precio"},"num": {"$sum":"$lineas.cantidad"}}}
-		#
-	])
-    	return template('aggregationPipelineProductos.tpl', result = result2, nombres = ["Nombre", "Numero", "Precio"], num = 10)
+			{"$unwind":"$lineas"},
+			# {"$project": {"precio": "$lineas.precio",
+			# 				"cantidad": "$lineas.cantidad",
+			# 				"nombre": "$lineas.nombre"}},
+			{"$match" : {"lineas.precio":{"$gte":float(request.query['min'])}}},
+			{"$group": {"_id": { "nombre": "$lineas.nombre", "precio": "$lineas.precio"},"num": {"$sum":"$lineas.cantidad"}}}
+			#
+		])
+	return template('aggregationPipelineProductos.tpl', result = result2, nombres = ["Nombre", "Numero", "Precio"], num = 10)
 
     
 @get('/age_range')
 # http://localhost:8080/age_range?min=80
 def agg3():
-    r = db.usuarios.aggregate( [
-		{"$group": {"_id": "$pais","num": {"$sum":1}, "max":{"$max":"$edad"}, "min":{"$min":"$edad"}}},
-		{"$project": {"range":{"$subtract":["$max", "$min"]}, "pais": "$_id", "max":"$max", "min":"$min"}},
-		{"$match" : {"range":{"$gte":int(request.query['min'])}}},
-		{"$sort" : {"range":-1, "_id":-1}}
-	])
-		return template('aggregationPipeline.tpl', result = r, claves= ["_id","range"], nombres = ["Pais", "Rango de Edades"])
-
-    
+    r = db.usuarios.aggregate([
+			{"$group": {"_id": "$pais","num": {"$sum":1}, "max":{"$max":"$edad"}, "min":{"$min":"$edad"}}},
+			{"$project": {"range":{"$subtract":["$max", "$min"]}, "pais": "$_id", "max":"$max","min":"$min"}},
+			{"$match" : {"range":{"$gte":int(request.query['min'])}}},
+			{"$sort" : {"range":-1, "_id":-1}}
+		])
+    return template('aggregationPipeline.tpl', result = r, claves= ["_id","range"], nombres = ["Pais", "Numero"], num = 10)    
     
 @get('/avg_lines')
 # http://localhost:8080/avg_lines
